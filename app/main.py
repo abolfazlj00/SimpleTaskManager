9ba903d13cee73aa5e78f22ce251e6f98d9c76ca
Abolfazl Jelodar <jelodarabolfazl@gmail.com>
2025-05-16 23:36:30 +0330

    Use Lifespan instead of on_event

--- app/main.py
+++ app/main.py
@@ -2,14 +2,20 @@ from fastapi import FastAPI
 from routers.tasks import router as task_manager_router
 from models import Base
 from database import engine
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Create Tables
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+app = FastAPI(
+    lifespan=lifespan
+)
+
 
 app.include_router(
     router=task_manager_router
 )
-
-@app.on_event("startup")
-async def startup():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
